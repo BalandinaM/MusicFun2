bd48b27ad3801baf697c674fc1f336b0e22ad832
BalandinaM <marina.balandina.89@mail.ru>
2026-06-08 23:16:38 +0500

    lesson2. PaginationType Offset

--- musicfun/src/features/tracks/api/tracksApi.ts
+++ musicfun/src/features/tracks/api/tracksApi.ts
@@ -21,6 +21,30 @@ export const tracksApi = baseApi.injectEndpoints({
         }
       },
     }),
+
+    //////paginationType: 'offset'////////
+
+    // fetchTracks: build.infiniteQuery<FetchTracksResponse, void, number>({
+    //   infiniteQueryOptions: {
+    //     initialPageParam: 1,
+    //     getNextPageParam: (lastPage, _allPages, lastPageParam) => {
+    //       return lastPageParam <
+    //         (lastPage.meta as { pagesCount: number }).pagesCount
+    //         ? lastPageParam + 1
+    //         : undefined
+    //     },
+    //   },
+    //   query: ({ pageParam }) => {
+    //     return {
+    //       url: 'playlists/tracks',
+    //       params: {
+    //         pageNumber: pageParam,
+    //         pageSize: 10,
+    //         paginationType: 'offset',
+    //       },
+    //     }
+    //   },
+    // }),
   }),
 })
 export const { useFetchTracksInfiniteQuery } = tracksApi
