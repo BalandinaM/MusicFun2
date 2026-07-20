f7bb24d5be29fdbfc9a7cbd5a3461c467fff01c4
BalandinaM <marina.balandina.89@mail.ru>
2026-07-21 00:09:36 +0500

    style: прокинуть в кнопку реакции, реакцию пользователя и покрасить иконку в розовый

--- musicfun/src/common/components/Reactions/Reactions.tsx
+++ musicfun/src/common/components/Reactions/Reactions.tsx
@@ -1,13 +1,11 @@
 import { LikeIcon, DislikeIcon } from '../Icons'
 import type { PlaylistAttributes } from '@/features/playlists/api'
 import s from './Reactions.module.css'
-import { CurrentUserReaction as UserReaction } from '@/common/enums'
-import type { CurrentUserReaction } from '@/common/types/types'
-import { useState } from 'react'
 import {
   useDislikePlaylistMutation,
   useLikePlaylistMutation,
 } from '@/features/playlists/api/playlistsApi'
+import type { CurrentUserReaction } from '@/common/types/types'
 
 type Props = {
   data: PlaylistAttributes
@@ -32,19 +30,54 @@ export const Reactions = ({ data, playlistId }: Props) => {
 
   return (
     <div>
-      <button
-        // className={isLiked === UserReaction.Like ? s.button : ''}
-        onClick={() => handleLikeReaction(playlistId)}
-      >
-        <LikeIcon />
-        <span>{data.likesCount}</span>
-      </button>
-      <button
-        onClick={() => handleDisLikeReaction(playlistId)}
-        //   className={isLiked === UserReaction.Dislike ? s.button : ''}
-      >
-        <DislikeIcon />
-      </button>
+      <ButtonReaction
+        variant="like"
+        playlistId={playlistId}
+        callback={handleLikeReaction}
+        userReaction={data.currentUserReaction}
+        likesCount={data.likesCount}
+      />
+      <ButtonReaction
+        variant="dislike"
+        playlistId={playlistId}
+        callback={handleDisLikeReaction}
+        userReaction={data.currentUserReaction}
+      />
     </div>
+  )
+}
+
+type PropsButton = {
+  variant: 'like' | 'dislike'
+  playlistId: string
+  callback: (playlistId: string) => void
+  userReaction: CurrentUserReaction
+  likesCount?: number
+}
+
+export const ButtonReaction = ({
+  variant,
+  playlistId,
+  callback,
+  userReaction,
+  likesCount,
+}: PropsButton) => {
+  const isActive =
+    (variant === 'like' && userReaction === 1) ||
+    (variant === 'dislike' && userReaction === -1)
+
+  return (
+    <button
+      onClick={() => callback(playlistId)}
+      className={isActive ? s.active : ''}
+    >
+      {variant === 'like' && (
+        <>
+          <LikeIcon />
+          <span>{likesCount}</span>
+        </>
+      )}
+      {variant === 'dislike' && <DislikeIcon />}
+    </button>
   )
 }
