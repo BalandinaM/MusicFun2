68ae191d9625257cf2fe0cee90aacd12e2052a39
BalandinaM <marina.balandina.89@mail.ru>
2026-07-23 15:46:12 +0500

    refactor: переместить MainPage в папку pages

--- musicfun/src/app/ui/index.ts
+++ musicfun/src/app/ui/index.ts
@@ -1,2 +1,1 @@
 export { default as App } from './App/App'
-export { MainPage } from './MainPage/MainPage'
--- musicfun/src/common/routing/Routing.tsx
+++ musicfun/src/common/routing/Routing.tsx
@@ -1,9 +1,9 @@
-import { MainPage } from '@/app/ui'
 import { OAuthCallback, ProfilePage } from '@/features/auth/iu'
 import { PlaylistsPage } from '@/features/playlists/ui'
 import { TracksPage } from '@/features/tracks/ui'
 import { Routes, Route } from 'react-router'
 import { PageNotFound } from '../components'
+import { MainPage } from '@/pages'
 
 export const Path = {
   //это предупреждение, вите советует убрать объявления контстант в другое место, а тут оставить только код компонента
