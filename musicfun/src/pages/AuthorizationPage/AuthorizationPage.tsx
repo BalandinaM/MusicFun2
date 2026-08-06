fd8739ada735befa3e46ea6ce462e6db47e0545c
BalandinaM <marina.balandina.89@mail.ru>
2026-08-06 10:46:14 +0500

    feat: страница авторизации

--- musicfun/src/pages/AuthorizationPage/AuthorizationPage.tsx
+++ musicfun/src/pages/AuthorizationPage/AuthorizationPage.tsx
@@ -1,15 +1,16 @@
 import { Path } from '@/common/routing'
 import { Login } from '@/features/auth/ui'
 import { NavLink } from 'react-router'
+import s from './AuthorizationPage.module.css'
 
-const AuthorizationPage = () => {
+export const AuthorizationPage = () => {
   return (
-    <div>
-      <h1>Millions of Songs. Free on Musifun.</h1>
+    <div className={s.wrapAuthorizationPage}>
+      <h1 className="text-heading-xl">Millions of Songs. Free on Musifun.</h1>
       <Login />
-      <NavLink to={Path.Main}>Сontinue without Sign In</NavLink>
+      <NavLink to={Path.Main} className={s.link}>
+        Сontinue without Sign In
+      </NavLink>
     </div>
   )
 }
-
-export default AuthorizationPage
