2fea5d6a33330dff35a19824686965163d3601c7
BalandinaM <marina.balandina.89@mail.ru>
2026-08-06 12:49:06 +0500

    feat: создать authSlice

---
+++ musicfun/src/features/auth/model/authSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    // user: MeResponse || null,
+    isLoggedIn: false,
+  },
+  selectors: {
+    selectIsLoggedIn: state => state.isLoggedIn,
+  },
+  reducers: create => ({
+    setIsLoggedInAC: create.reducer<{ isLoggedIn: boolean }>(
+      (state, action) => {
+        state.isLoggedIn = action.payload.isLoggedIn
+      }
+    ),
+  }),
+})
+
+export const { selectIsLoggedIn } = authSlice.selectors
+export const { setIsLoggedInAC } = authSlice.actions
+export const authReducer = authSlice.reducer
