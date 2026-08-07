646b5e7f05b09194028e2eb3686e275a8f9033c4
BalandinaM <marina.balandina.89@mail.ru>
2026-08-07 09:45:18 +0500

    style: добавить фоновую картинку на страницу авторизации

--- musicfun/src/pages/AuthorizationPage/AuthorizationPage.tsx
+++ musicfun/src/pages/AuthorizationPage/AuthorizationPage.tsx
@@ -5,6 +5,8 @@ import s from './AuthorizationPage.module.css'
 import { useAppSelector } from '@/common/hooks'
 import { selectIsLoggedIn } from '@/features/auth/model/authSlice'
 import { Button } from '@/common/components'
+import backgroundImage from '@/assets/images/authCover.png'
+import { SmileIcon } from '../../common/components/Icons/SmileIcon'
 
 export const AuthorizationPage = () => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
@@ -20,6 +22,16 @@ export const AuthorizationPage = () => {
 
   return (
     <div className={s.wrapAuthorizationPage}>
+      <div className={s.containerImg}>
+        <img
+          src={backgroundImage}
+          alt="Background"
+          className={s.bgImage}
+          width={360}
+          height={340}
+        />
+        <SmileIcon className={s.overlay} />
+      </div>
       <h1 className="text-heading-xl">Millions of Songs. Free on Musifun.</h1>
       <Login />
       <Button
