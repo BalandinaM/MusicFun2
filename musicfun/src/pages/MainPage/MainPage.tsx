460025e44a566a627be9429599cdc2cd1d87f563
BalandinaM <marina.balandina.89@mail.ru>
2026-08-10 00:51:25 +0500

    feat: Добавить модалку авторизации поверх MainPage и обновить auth flow

--- musicfun/src/pages/MainPage/MainPage.tsx
+++ musicfun/src/pages/MainPage/MainPage.tsx
@@ -7,6 +7,8 @@ import { useFetchPlaylistsQuery } from '@/features/playlists/api/playlistsApi'
 import { CardPlaylist } from '@/features/playlists/ui'
 import { useAppSelector } from '@/common/hooks'
 import { selectIsLoggedIn } from '@/features/auth/model/authSlice'
+import { AuthorizationModal } from '@/features/auth/ui/AuthorizationModal'
+import { useState } from 'react'
 
 const tags = ['Playlists', 'Artists', 'Albums', 'Podcast', 'Podcasts & shows']
 
@@ -17,32 +19,40 @@ export const MainPage = () => {
     useFetchTracksListQuery({})
   const { data: playlists, isLoading: isLoadingPlaylists } =
     useFetchPlaylistsQuery({})
-
-  const included = newTracks?.included
+  const [showAuthModal, setShowAuthModal] = useState(true)
   const isAuth = useAppSelector(selectIsLoggedIn)
 
-  if (isLoadingTracks || isLoadingPlaylists) return <div>Loading...</div>
+  const included = newTracks?.included
+
+  const isPageLoading = isLoadingTracks || isLoadingPlaylists
+
+  if (!showAuthModal && isPageLoading) return <div>Loading...</div>
 
   return (
-    <div className={s.wrapMainPage}>
-      <HeaderMainPage data={data} />
-      <HorizontalScroll gap={8} padding="12px 0">
-        {tags.map(tag => (
-          <Chip label={tag} key={tag} isAuth={isAuth} />
-        ))}
-      </HorizontalScroll>
-      <h2>New playlists</h2>
-      <HorizontalScroll>
-        {playlists?.data.map(playlist => (
-          <CardPlaylist key={playlist.id} playlist={playlist} />
-        ))}
-      </HorizontalScroll>
-      <h2>New Tracks</h2>
-      <HorizontalScroll gap={8} padding="12px 0">
-        {newTracks?.data.map(track => (
-          <CardTrack track={track} included={included} />
-        ))}
-      </HorizontalScroll>
-    </div>
+    <>
+      {showAuthModal && !isAuth && (
+        <AuthorizationModal onClose={() => setShowAuthModal(false)} />
+      )}
+      <div className={s.wrapMainPage}>
+        <HeaderMainPage data={data} />
+        <HorizontalScroll gap={8} padding="12px 0">
+          {tags.map(tag => (
+            <Chip label={tag} key={tag} isAuth={isAuth} />
+          ))}
+        </HorizontalScroll>
+        <h2>New playlists</h2>
+        <HorizontalScroll>
+          {playlists?.data.map(playlist => (
+            <CardPlaylist key={playlist.id} playlist={playlist} />
+          ))}
+        </HorizontalScroll>
+        <h2>New Tracks</h2>
+        <HorizontalScroll gap={8} padding="12px 0">
+          {newTracks?.data.map(track => (
+            <CardTrack track={track} included={included} />
+          ))}
+        </HorizontalScroll>
+      </div>
+    </>
   )
 }
